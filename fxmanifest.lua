67fe822a6c4b89b8b85d9c5d57624ef05ad7a86a
mOntey <77300926+mOnteySZEF@users.noreply.github.com>
2026-03-18 00:33:51 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/style.css',
+    'html/main.js',
+    'html/index.html',
+}
